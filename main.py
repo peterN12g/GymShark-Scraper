7240dec8cf94292c583ddb2a30da57f52b72e76c
Peter <peternguyensmhs@gmail.com>
2024-02-15 22:55:16 -0600

    added legacy tee

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
+import json
 import requests
 from bs4 import BeautifulSoup
-import http.client, urllib
+import http.client
+import urllib
 from dotenv import load_dotenv
 import os
 
-url = "https://www.gymshark.com/products/gymshark-react-5-short-black-ss23"
-req = requests.get(url)
-soup = BeautifulSoup(req.content, "html.parser")
-
 load_dotenv()
-USER_TOKEN = os.environ.get('USER_TOKEN')
-API_KEY = os.environ.get('API_KEY')
+USER_TOKEN = os.environ.get(USER_TOKEN)
+API_KEY = os.environ.get(API_KEY)
 
-#Finding Product Information from HTML
-title_tag = soup.find("title")
-name = title_tag.text if title_tag else "Name not found"
-print(f"Product Name: {name}")
-
-meta_tag = soup.find("meta", attrs={"name": "twitter:data1"})
-price = meta_tag["content"] if meta_tag else "Price not found"
-curr_price = 36
-#remove $ from price to convert int
-priceConv = ''.join(c for c in price if c.isdigit() or c == '.')
-priceInt = int(priceConv)
-print(f"Product Price: {price}")
-if priceInt is not None and priceInt < curr_price:
-    #Use Pushover app for Push Notifications
+def send_notification(name, price):
     conn = http.client.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
-    urllib.parse.urlencode({
-        "token": "USER_TOKEN",
-        "user": "API_KEY",
-        "message": f"Price drop: {name} - {price}!",
-    }), { "Content-type": "application/x-www-form-urlencoded" })
+                 urllib.parse.urlencode({
+                     "token": API_KEY,
+                     "user": USER_TOKEN,
+                     "message": f"Price drop: {name} - {price}!",
+                 }), {"Content-type": "application/x-www-form-urlencoded"})
     response = conn.getresponse()
 
+def scrape_and_notify():
+    url_black_shorts = "https://www.gymshark.com/products/gymshark-react-5-short-black-ss23"
+    url_legacy_tee_grey = "https://www.gymshark.com/products/gymshark-legacy-t-shirt-pebble-grey-ss24"
+    req_black_shorts = requests.get(url_black_shorts)
+    req_legacy_tee_grey = requests.get(url_legacy_tee_grey)
+    soup_black_shorts = BeautifulSoup(req_black_shorts.content, "html.parser")
+    soup_legacy_tee_grey = BeautifulSoup(req_legacy_tee_grey.content, "html.parser")
 
+    # Finding Product Information from HTML
+    title_tag = soup_black_shorts.find("title")
+    name_black_shorts = title_tag.text if title_tag else "Name not found"
+    name_legacy_tee_grey = title_tag.text if title_tag else "Name not found"
 
+    meta_tag = soup_black_shorts.find("meta", attrs={"name": "twitter:data1"})
+    price_black_shorts = meta_tag["content"] if meta_tag else "Price not found"
+    price_legacy_tee_grey = meta_tag["content"] if meta_tag else "Price not found"
+    curr_price_black_shorts = 36
+    curr_price_legacy_tee_grey = 28
+    target_price_legacy_tee_grey = (curr_price_legacy_tee_grey - 28 * 0.25)
+    target_price_black_shorts = (curr_price_black_shorts - 36 * 0.25)
+    # remove $ from price to convert int
+    priceConv_black_shorts = ''.join(c for c in price_black_shorts if c.isdigit())
+    priceInt_black_shorts = int(priceConv_black_shorts)
+    priceConv_legacy_tee_grey = ''.join(c for c in price_legacy_tee_grey if c.isdigit())
+    priceInt_legacy_tee_grey = int(priceConv_legacy_tee_grey)
+
+    if priceInt_black_shorts is not None and priceInt_black_shorts < target_price_black_shorts:
+        send_notification(name_black_shorts, price_black_shorts)
+        print("Notification sent!")
+    
+    if priceInt_legacy_tee_grey is not None and priceInt_legacy_tee_grey < target_price_legacy_tee_grey:
+        send_notification(name_legacy_tee_grey, price_legacy_tee_grey)
+        print("Notification sent!")
+
+if __name__ == "__main__":
+    scrape_and_notify()
