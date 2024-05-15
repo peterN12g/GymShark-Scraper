00581704e3b95be785748c0e08fb19281dfbe061
peterN12g <peternguyensmhs@gmail.com>
2024-05-15 00:23:31 -0500

    request notifi permi (doesnt send notification after accept)

--- client/public/sw-dependencies.js
+++ client/public/sw-dependencies.js
@@ -1,11 +1,27 @@
  const checkPermission = () => {
     if (!('serviceWorker' in navigator))
         throw new Error('Service worker permission not supported!')
+   
+    if(!('Notification' in window)){
+      throw new Error('No support for notofication API')
+    }
  }
 
  const registerSW = async () => {
     const registration = await navigator.serviceWorker.register('/sw.js')
     return registration
  }
+
+ const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission()
+
+    if (permission != 'granted'){
+      throw new Error('Notification permission not granted')
+    }else{
+      new Notification('Test')
+    }
+    
+ }
  checkPermission()
  registerSW()
+ requestNotificationPermission()
