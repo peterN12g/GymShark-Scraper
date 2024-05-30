7bb8688dae439df2f67c0a13596ed3c88eead1dd
peterN12g <peternguyensmhs@gmail.com>
2024-05-29 21:13:25 -0500

    debug mixed content error

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
       }
     }
 
-    fetch("http://34.122.224.207/members")
+    fetch("https://34.122.224.207/members")
       .then(res => res.json())
       .then(data => {
         setData(data.product);
