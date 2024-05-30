4e7aa541422be4271d476cba1c001a7b15693efd
peterN12g <peternguyensmhs@gmail.com>
2024-05-29 21:26:49 -0500

    test debug MIME error

---
+++ client/public/sw-dependencies.js
@@ -0,0 +1,25 @@
+ export const checkPermission = () => {
+    if (!('serviceWorker' in navigator))
+        throw new Error('Service worker permission not supported!')
+  }
+    if(!('Notification' in window)){
+      throw new Error('No support for notofication API')
+    }
+
+ export const registerSW = async () => {
+    const registration = await navigator.serviceWorker.register('/sw.js')
+    return registration
+ }
+
+ export const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission()
+    if (permission !== 'granted'){
+      throw new Error('Notification permission not granted')
+    }else{
+      new Notification('Notifications Granted!')
+    }
+    
+ }
+ checkPermission()
+ registerSW()
+ requestNotificationPermission()
